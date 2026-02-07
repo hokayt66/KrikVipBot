4ed7f54621b3de7a4d8bd0ef0a0c9002539ba5f0
hokayt66 <hokayt688@gmail.com>
2026-02-07 18:09:15 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -40,8 +40,8 @@ async def buttons(call: CallbackQuery):
 
 
 async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
-
 
 if __name__ == "__main__":
     asyncio.run(main())
