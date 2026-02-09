8d9c6ac8a839e0845574a9ce986a4adc43d27ca4
hokayt66 <hokayt688@gmail.com>
2026-02-09 10:03:58 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,12 @@
 import os
 import asyncio
 from aiogram import Bot, Dispatcher, F
-from aiogram.types import Message, CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import (
+    Message,
+    CallbackQuery,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton
+)
 from aiogram.filters import Command
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
@@ -12,36 +17,44 @@ if not BOT_TOKEN:
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
+# ✅ ID твоего стикера
+STICKER_ID = "CAACAgIAAxkBAAIXo2mJlDUnJgJtip4xMw6mOz75nLKCAAKtcQACB4pYS9zy4G9qyrjcOgQ"
+
 
 @dp.message(Command("start"))
 async def start(message: Message):
-    keyboard = InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="Кнопка 1", callback_data="btn_1")],
-        [InlineKeyboardButton(text="Кнопка 2", callback_data="btn_2")],
-        [InlineKeyboardButton(text="Кнопка 3", callback_data="btn_3")],
-    ])
+    # Отправляем стикер
+    await message.answer_sticker(STICKER_ID)
 
+    # Кнопка товара
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(
+                    text="🧊 A-PVP 🧊",
+                    callback_data="product_apvp"
+                )
+            ]
+        ]
+    )
+
+    # Текст + кнопка
     await message.answer(
-        "Привет 👋\nВыбери кнопку ниже:",
+        "Выберите товар 📞:",
         reply_markup=keyboard
     )
 
 
-@dp.callback_query(F.data.startswith("btn_"))
-async def buttons(call: CallbackQuery):
-    texts = {
-        "btn_1": "Ты нажал Кнопку 1",
-        "btn_2": "Ты нажал Кнопку 2",
-        "btn_3": "Ты нажал Кнопку 3",
-    }
-
-    await call.message.edit_text(texts.get(call.data, "Неизвестная кнопка"))
+@dp.callback_query(F.data == "product_apvp")
+async def apvp_product(call: CallbackQuery):
+    await call.message.edit_text("Вы выбрали 🧊 A-PVP 🧊")
     await call.answer()
 
 
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
